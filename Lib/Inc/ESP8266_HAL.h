3d365ac15133a17409904da39153d63935781bcd
Simon Waiz <simonwan5577@gmail.com>
2026-05-01 19:52:57 +0800

    delete unnecessary code

--- Lib/Inc/ESP8266_HAL.h
+++ Lib/Inc/ESP8266_HAL.h
@@ -12,10 +12,8 @@
 
 
 void ESP_Init (char *SSID, char *PASSWD);
-// void Server_Program (void);
 int HTTP_GET_Request(char *host, int port, char *path);
 int HTTP_POST_Request(char *host, int port, char *path);
-// void send_simple_get(const char* format_path, char* out_buf);
 int Wttr_Get_Weather(void);
 
 #endif /* INC_ESP8266_HAL_H_ */
