bfbc5fd9ba40ae5378051510871e0a7610600374
w41z <simonwan5577@gmail.com>
2026-05-06 12:34:27 +0800

    Comment out RGB functions and include statement

--- Lib/Src/rgb.c
+++ Lib/Src/rgb.c
@@ -1,46 +1,49 @@
-#include "rgb.h"
+/* Depreciated
+   This is just a test function created for testing the VScode environment */
 
-static uint8_t color_state = 0;
+// #include "rgb.h"
 
-void RGB_Init() {
-    // Initial every pin to high -> LED goes off
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
-}
+// static uint8_t color_state = 0;
 
-void Led_toggle_RGB() {    
-    // the reading from pa0 = high -> button has been press, perform the color-switching
-    if (HAL_GPIO_ReadPin(K1_GPIO_Port, K1_Pin) == GPIO_PIN_SET)
-    {
-      color_state++;
-      if (color_state > 3) color_state = 1;
+// void RGB_Init() {
+//     // Initial every pin to high -> LED goes off
+//     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+//     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+//     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+// }
 
-      switch (color_state)
-      {
-        case 1://R
-          HAL_GPIO_WritePin(GPIOB, R_Pin, GPIO_PIN_RESET);
-          HAL_GPIO_WritePin(GPIOB, G_Pin, GPIO_PIN_SET);
-          HAL_GPIO_WritePin(GPIOB, B_Pin, GPIO_PIN_SET);
-          break;
-        case 2://G
-          HAL_GPIO_WritePin(GPIOB, R_Pin, GPIO_PIN_SET);
-          HAL_GPIO_WritePin(GPIOB, G_Pin, GPIO_PIN_RESET);
-          HAL_GPIO_WritePin(GPIOB, B_Pin, GPIO_PIN_SET);
-          break;
-        case 3://B
-          HAL_GPIO_WritePin(GPIOB, R_Pin, GPIO_PIN_SET);
-          HAL_GPIO_WritePin(GPIOB, G_Pin, GPIO_PIN_SET);
-          HAL_GPIO_WritePin(GPIOB, B_Pin, GPIO_PIN_RESET);
-          break;
+// void Led_toggle_RGB() {    
+//     // the reading from pa0 = high -> button has been press, perform the color-switching
+//     if (HAL_GPIO_ReadPin(K1_GPIO_Port, K1_Pin) == GPIO_PIN_SET)
+//     {
+//       color_state++;
+//       if (color_state > 3) color_state = 1;
 
-      }
-    }
-    // wait for user to release the button
-    while (HAL_GPIO_ReadPin(K1_GPIO_Port, K1_Pin) == 1)
-    {
-      HAL_Delay(10);
-    }
-    // extra delay for stability
-    HAL_Delay(10);
-}
+//       switch (color_state)
+//       {
+//         case 1://R
+//           HAL_GPIO_WritePin(GPIOB, R_Pin, GPIO_PIN_RESET);
+//           HAL_GPIO_WritePin(GPIOB, G_Pin, GPIO_PIN_SET);
+//           HAL_GPIO_WritePin(GPIOB, B_Pin, GPIO_PIN_SET);
+//           break;
+//         case 2://G
+//           HAL_GPIO_WritePin(GPIOB, R_Pin, GPIO_PIN_SET);
+//           HAL_GPIO_WritePin(GPIOB, G_Pin, GPIO_PIN_RESET);
+//           HAL_GPIO_WritePin(GPIOB, B_Pin, GPIO_PIN_SET);
+//           break;
+//         case 3://B
+//           HAL_GPIO_WritePin(GPIOB, R_Pin, GPIO_PIN_SET);
+//           HAL_GPIO_WritePin(GPIOB, G_Pin, GPIO_PIN_SET);
+//           HAL_GPIO_WritePin(GPIOB, B_Pin, GPIO_PIN_RESET);
+//           break;
+
+//       }
+//     }
+//     // wait for user to release the button
+//     while (HAL_GPIO_ReadPin(K1_GPIO_Port, K1_Pin) == 1)
+//     {
+//       HAL_Delay(10);
+//     }
+//     // extra delay for stability
+//     HAL_Delay(10);
+// }
